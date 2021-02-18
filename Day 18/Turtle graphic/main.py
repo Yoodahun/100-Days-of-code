837ec53d10d083bc5dfaf7d7b3d90771bdd5b762
Yoodahun <dahun4032@gmail.com>
2021-02-19 00:27:31 +0900

    Day 18 - commit 3

--- Day 18/Turtle graphic/main.py
+++ Day 18/Turtle graphic/main.py
@@ -1,12 +1,17 @@
 from turtle import Turtle, Screen
+import random
 
 the_turtle = Turtle()
 the_turtle.shape("turtle")
 the_turtle.color("purple")
 
-for i in range(3, 20):
-    angle = 360 / i
+color = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray"]
+
+for i in range(3, 10): #number of side
+    angle = 360 / i #corner angle
+    the_turtle.color(random.choice(color))
     for _ in range(i):
+
         the_turtle.forward(50)
         the_turtle.right(angle)
 
