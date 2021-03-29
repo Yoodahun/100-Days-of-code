fbdc641119fb57af96d2426cda566ae9162180a3
Yoodahun <dahun4032@gmail.com>
2021-03-30 00:37:39 +0900

    Day 33 - commit 2

--- Day 33/main.py
+++ Day 33/main.py
@@ -1,4 +1,34 @@
 import requests
+import datetime as dt
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response.json())
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+# print(data)
+# print(iss_position)
+
+# https://api.sunrise-sunset.org/json
+
+
+parameter = {
+    "lat" : 33.590355,
+    "lng" : 130.401718,
+    "formatted": 0
+
+}
+response = requests.get("https://api.sunrise-sunset.org/json",params=parameter)
+response.raise_for_status()
+sunrise = response.json()["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = response.json()["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+
+time_now = dt.datetime.now()
+
+
+print(time_now.hour)
