22ef0fcf5f19bc2f12bf400b61cf0270a58758eb
Yoodahun <dahun4032@gmail.com>
2021-04-08 01:09:21 +0900

    Day 35 - commit 2

--- Day 35/WeatherForecast/main.py
+++ Day 35/WeatherForecast/main.py
@@ -8,10 +8,26 @@ lon = 130.401718
 weather_params = {
     "lat":lat,
     "lon":lon,
-    "appid":API_KEY
+    "appid":API_KEY,
+    "exclude": "current,minutely,daily"
 }
 
 # response = requests.get(f"{END_POINT}?lat={lat}&lon={lon}&exclude=&appid={API_KEY}")
 response = requests.get(END_POINT, params=weather_params)
-print(response.status_code)
-print(response.json()["hourly"])
+response.raise_for_status()
+weather_data = response.json()["hourly"][:12]
+
+for hour in weather_data:
+    print(hour)
+    if hour["weather"][0]["id"] < 700:
+        print("Bring an Umbrella")
+
+
+
+# for hour in range(0,12):
+#     print(hour)
+#     if weather_data[hour]["weather"][0]["id"] < 700:
+#         print("Bring an Umbrella")
+
+# print(weather_data["hourly"])
+# hourly[0][weather][0][id]
