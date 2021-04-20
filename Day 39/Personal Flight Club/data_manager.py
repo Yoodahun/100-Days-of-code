8eb24a13bbc68a6bffcd5b5e3535b7f87d757f04
Yoodahun <dahun4032@gmail.com>
2021-04-21 01:08:44 +0900

    Day 39 commit 2

--- Day 39/Personal Flight Club/data_manager.py
+++ Day 39/Personal Flight Club/data_manager.py
@@ -1,3 +1,23 @@
+import requests
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+
+    def __init__(self):
+        self.GOOGLE_SHEET_KEY = ""
+        self.GOOGLE_SHEET_API_ENDPOINT = "https://api.sheety.co/9f9b62b3fef9c5050eef57ceb4e58bda/copyOfFlightDeals/prices"
+        self.google_sheet_header = {
+            "Authorization": f"Bearer {self.GOOGLE_SHEET_KEY}"
+        }
+        self.body = {}
+
+    def updateFlightData(self, data):
+        self.body["price"] = data
+
+        response = requests.put(
+            url=f"{self.GOOGLE_SHEET_API_ENDPOINT}/{data['id']}",
+            headers=self.google_sheet_header,
+            json=self.body
+        )
+        response.raise_for_status()
--- Day 39/Personal Flight Club/main.py
+++ Day 39/Personal Flight Club/main.py
@@ -1,1 +1,35 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import requests
+from pprint import pprint
+from flight_search import FlightSearch
+from data_manager import DataManager
+
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
+# program requirements.
+flight_search = FlightSearch()
+data_manager = DataManager()
+
+
+response = requests.get(
+    url=data_manager.GOOGLE_SHEET_API_ENDPOINT,
+    headers=data_manager.google_sheet_header
+)
+
+response.raise_for_status()
+sheet_data = response.json()["prices"]
+pprint(response.json()["prices"])
+
+
+
+for data in sheet_data:
+
+    if data["iataCode"] == "":
+        data["iataCode"] = flight_search.getIataCode()
+
+    data_manager.updateFlightData(data)
+
+
+response = requests.get(
+    url=data_manager.GOOGLE_SHEET_API_ENDPOINT,
+    headers=data_manager.google_sheet_header
+)
+pprint(response.json()["prices"])
