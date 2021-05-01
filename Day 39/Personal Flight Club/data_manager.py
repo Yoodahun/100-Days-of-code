32b0f219965f86b384bb1aed1780475dfdc5b908
Yoodahun <dahun4032@gmail.com>
2021-05-01 12:46:32 +0900

    Day 40 commit 1

--- Day 39/Personal Flight Club/data_manager.py
+++ Day 39/Personal Flight Club/data_manager.py
@@ -6,7 +6,8 @@ class DataManager:
 
     def __init__(self):
         self.GOOGLE_SHEET_KEY = ""
-        self.GOOGLE_SHEET_API_ENDPOINT = "https://api.sheety.co/9f9b62b3fef9c5050eef57ceb4e58bda/copyOfFlightDeals/prices"
+        self.GOOGLE_PRICE_SHEET_API_ENDPOINT = "https://api.sheety.co/9f9b62b3fef9c5050eef57ceb4e58bda/copyOfFlightDeals/prices"
+        self.GOOGLE_USER_SHEET_API_ENDPOINT = "https://api.sheety.co/9f9b62b3fef9c5050eef57ceb4e58bda/copyOfFlightDeals/users"
         self.google_sheet_header = {
             "Authorization": f"Bearer {self.GOOGLE_SHEET_KEY}"
         }
@@ -16,8 +17,32 @@ class DataManager:
         self.body["price"] = data
 
         response = requests.put(
-            url=f"{self.GOOGLE_SHEET_API_ENDPOINT}/{data['id']}",
+            url=f"{self.GOOGLE_PRICE_SHEET_API_ENDPOINT}/{data['id']}",
             headers=self.google_sheet_header,
             json=self.body
         )
         response.raise_for_status()
+
+    def add_excel_user_data(self, first_name, last_name, email):
+
+        # response = requests.get(
+        #     url=self.GOOGLE_USER_SHEET_API_ENDPOINT,
+        #     headers=self.google_sheet_header
+        # )
+
+        body = {}
+        body["user"] = {}
+        print(body)
+
+        body["user"]["firstName"] = first_name
+        body["user"]["lastName"] = last_name
+        body["user"]["email"] = email
+
+        response = requests.post(
+            url=self.GOOGLE_USER_SHEET_API_ENDPOINT,
+            headers=self.google_sheet_header,
+            json=body
+        )
+        response.raise_for_status()
+        print(response.json())
+        print("You're in the club!")
--- Day 39/Personal Flight Club/main.py
+++ Day 39/Personal Flight Club/main.py
@@ -4,53 +4,65 @@ from flight_search import FlightSearch
 from data_manager import DataManager
 from notification_manager import NotificationManager
 
-ORIGIN_CITY_DEPARTURE = "LON"
-
-# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
-# program requirements.
+# ORIGIN_CITY_DEPARTURE = "LON"
+#
+# # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
+# # program requirements.
 flight_search = FlightSearch()
 data_manager = DataManager()
 notification_manager = NotificationManager()
+#
+# response = requests.get(
+#     url=data_manager.GOOGLE_PRICE_SHEET_API_ENDPOINT,
+#     headers=data_manager.google_sheet_header
+# )
+#
+# response.raise_for_status()
+# sheet_data = response.json()["prices"]
+# pprint(response.json()["prices"])
+#
+# for data in sheet_data:
+#
+#     if data["iataCode"] == "":
+#         data["iataCode"] = flight_search.get_iataCode(data["city"])
+#
+#     data_manager.update_excel_flight_data(data)
+#
+# response = requests.get(
+#     url=data_manager.GOOGLE_PRICE_SHEET_API_ENDPOINT,
+#     headers=data_manager.google_sheet_header
+# )
+#
+# for data in response.json()["prices"]:
+#     search_result = flight_search.get_flight_data(
+#         ORIGIN_CITY_DEPARTURE,
+#         data
+#     )
+#
+#     if search_result is None:
+#         continue
+#
+#     if search_result.price < data["lowestPrice"]:
+#         notification_manager.notification(
+#             price=search_result.price,
+#             origin_city= search_result.departure_city,
+#             origin_airport= search_result.departure_airport_code,
+#             destination_city= search_result.arrive_city,
+#             destination_airport= search_result.arrive_airport_code,
+#             out_date=search_result.out_date,
+#             return_date=search_result.return_date
+#         )
+#
 
-response = requests.get(
-    url=data_manager.GOOGLE_SHEET_API_ENDPOINT,
-    headers=data_manager.google_sheet_header
-)
+print("Welcome to Dahun's Flight Club.")
+print("We find the best flight deals and email you.")
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+email = input("What is your email? ")
+check_email = input("Type your email again. ")
 
-response.raise_for_status()
-sheet_data = response.json()["prices"]
-pprint(response.json()["prices"])
+while email != check_email:
+    check_email = input("Not matched. Type your email again. ")
 
-for data in sheet_data:
-
-    if data["iataCode"] == "":
-        data["iataCode"] = flight_search.get_iataCode(data["city"])
-
-    data_manager.update_excel_flight_data(data)
-
-response = requests.get(
-    url=data_manager.GOOGLE_SHEET_API_ENDPOINT,
-    headers=data_manager.google_sheet_header
-)
-
-for data in response.json()["prices"]:
-    search_result = flight_search.get_flight_data(
-        ORIGIN_CITY_DEPARTURE,
-        data
-    )
-
-    if search_result is None:
-        continue
-
-    if search_result.price < data["lowestPrice"]:
-        notification_manager.notification(
-            price=search_result.price,
-            origin_city= search_result.departure_city,
-            origin_airport= search_result.departure_airport_code,
-            destination_city= search_result.arrive_city,
-            destination_airport= search_result.arrive_airport_code,
-            out_date=search_result.out_date,
-            return_date=search_result.return_date
-        )
-
+data_manager.add_excel_user_data(first_name, last_name, email)
 
