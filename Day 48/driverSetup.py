9c848537d4d7f2eef9dc579976609a5263f0b087
Yoodahun <dahun4032@gmail.com>
2021-07-03 10:58:36 +0900

    ADD fill the form

--- Day 48/driverSetup.py
+++ Day 48/driverSetup.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 
+
 chromedriver_path = "/Users/yoodahun/Documents/Github/Python/100 Days of Code-CLI/chromedriver"
 
 
 class DriverSetup():
     def __init__(self):
-        self.chromeOption = webdriver.ChromeOptions()
-        self.chromeOption.headless = True
-        self.driver = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=self.chromeOption)
+        # self.chromeOption = webdriver.ChromeOptions()
+        # self.chromeOption.headless = True
+        self.driver = webdriver.Chrome(executable_path=chromedriver_path)
 
     def setupDriver(self) -> webdriver:
         return self.driver
+
+    def quitDriver(self):
+        self.driver.quit()
--- Day 48/interaction.py
+++ Day 48/interaction.py
@@ -1,9 +1,28 @@
 from driverSetup import DriverSetup
+from selenium.webdriver.common.keys import Keys
 
 driver = DriverSetup().setupDriver()
-wikipedia_page = "https://en.wikipedia.org/wiki/Main_Page"
-driver.get(wikipedia_page)
+# wikipedia_page = "https://en.wikipedia.org/wiki/Main_Page"
+# driver.get(wikipedia_page)
+#
+# # wikipedia_statistics = driver.find_element_by_xpath('//*[@id="articlecount"]/a[1]')
+# # wikipedia_statistics.click()
+# # wikipedia_statistics.find_element_by_link_text()
+#
+# search = driver.find_element_by_name("search")
+# search.send_keys("Python")
+# search.send_keys(Keys.ENTER)
+# # driver.quit()
 
-wikipedia_statistics = driver.find_element_by_xpath('//*[@id="articlecount"]/a[1]').text
-print(wikipedia_statistics)
+the_lab_report = "http://secure-retreat-92358.herokuapp.com/"
+driver.get(the_lab_report)
 
+first_name = driver.find_element_by_name("fName")
+last_name = driver.find_element_by_name("lName")
+email = driver.find_element_by_name("email")
+
+first_name.send_keys("TEST")
+last_name.send_keys("PANDA")
+email.send_keys("test@gmail.com")
+button = driver.find_element_by_xpath("//button[@type='submit']")
+button.click()
